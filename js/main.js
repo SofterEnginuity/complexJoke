90346b98c9ccb6119549302790c4cde9f393867f
Marqae Morris <mamorris920@gmail.com>
2024-10-21 21:20:08 -0400

    bug fix

--- js/main.js
+++ js/main.js
@@ -11,14 +11,13 @@ let jokeType = document.querySelector('#jokeType').value
 
 console.log(jokeType)
 // const userInput = document.querySelector('input').value 
+
 fetch(`https://v2.jokeapi.dev/joke/Any?safe-mode&type=${jokeType}`)
 .then(res => res.json())
 .then(data => {
     console.log(data)
-
-    //https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky?blacklistFlags=nsfw,religious,political,racist,sexist,explicit
-
-   if(data.type === 'single'){
+    
+    if(data.type === 'single'){
     document.querySelector('#originalJoke').innerText = data.joke 
     document.querySelector('#setup').innerText = ""
     document.querySelector('#delivery').innerText = ""
@@ -27,44 +26,34 @@ fetch(`https://v2.jokeapi.dev/joke/Any?safe-mode&type=${jokeType}`)
     document.querySelector('#setup').innerText = data.setup 
     document.querySelector('#originalJoke').innerText = ""
   }
-  getTranslation()
+ 
+ let joke = data.joke
+
+
+  const API_KEY = 'AIzaSyBUrq4lhHGnO67IgFoJ6dJicVUiu9PJYDI';
+  const sourceLang = 'en';
+  const targetLang = 'es';
+  const urlDos = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&source=${sourceLang}&target=${targetLang}&q=${joke}`;
+   
+  fetch(urlDos)
+  .then(res => res.json())
+  .then(data => {
+        console.log(data.data.translations[0].translatedText)
+
+    document.querySelector('#translatedJoke').innerText = data.data.translations[0].translatedText
+  })
+ 
+
+//   if( document.querySelector('#translatedJoke').innerText = data.data.translations[0].translatedText){
+// document.querySelector('#originalJoke').classList.remove('originalJoke') 
+//     document.querySelector('#originalJoke').classList.add('originalJoke')
+//   }else{
+//  console.log(`it's working`)
+//   }
+  
+
 
 })
+
 }
 // // document.querySelector('#originalJoke').innerHTML
- function getTranslation(text){
-
-
-     const url = `https://655.mtis.workers.dev/translate?text=Tell%20me%20a%20joke%20about%20Cloudflare&source_lang=en&target_lang=fr`
-    fetch(url, {
-        mode: 'no-cors',
-        headers: {
-          'Access-Control-Allow-Origin':'*'
-        }
-      })
-    .then(res => res.json())
-    .then(data2 =>{
-        console.log(data2)
-    })
-   .catch(err => {
-    console.log(`error ${err}`)
-})
-}
-
-
-
-
-
-
-
-
-
-
-
-// })
-// })
-
-
-// document.querySelector('').innerText = data.setup
-
-//  document.querySelector('translatedJoke').innerText = ????
